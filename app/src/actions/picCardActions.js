6cd331f250c604033ab3fe708fe004ed2f965ebe
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-06-22 19:22:41 -0400

    picCardActions - toggleEditing, updatePic

---
+++ app/src/actions/picCardActions.js
@@ -0,0 +1,16 @@
+export const TOGGLE_PIC = "TOGGLE_EDITING";
+eposrt const UPDATE_PIC = "UPDATE_PIC";
+
+export const toggleEditing = () => {
+  return {
+    type: TOGGLE_PIC
+  }
+};
+
+export const updatePic = (newPic) => {
+  return{
+    type: UPDATE_PIC,
+    payload: newPic
+  }
+};
+
